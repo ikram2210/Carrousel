8bf0a882935b978870a1ac03b08df8799671ca71
ikram2210 <69350259+ikram2210@users.noreply.github.com>
2020-09-18 11:57:46 +0200

    Add files via upload

---
+++ carrousel.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+var $carrousel = $('#carrousel'), 
+    $img = $('#carrousel img'),
+    $points = $('.cercle') ,
+    indexImg = $img.length - 1, 
+    i = 0, 
+    $currentImg = $img.eq(i); 
+    $currentPoint = $points.eq(i);
+
+    $img.css('display','none'); 
+    $currentImg.css('display','block');
+    $currentPoint.css('background','blue');
+
+
+    $('.next').click(function(){
+    $('.cercle').css('background','white');
+
+    i++; 
+
+    if( i <= indexImg ){
+        $img.css('display','none'); 
+        $currentImg = $img.eq(i); 
+        $currentPoint = $points.eq(i);
+        $currentImg.css('display', 'block');
+        $currentPoint.css('background','blue');
+    }
+    else{
+        i = indexImg;   
+    }
+
+});
+
+$('.prev').click(function(){ 
+$('.cercle').css('background','white');
+
+    i--; 
+    if( i >= 0 ){
+        $img.css('display','none');
+        $currentImg = $img.eq(i);
+        $currentPoint = $points.eq(i);
+        $currentImg.css('display','block');
+        $currentPoint.css('background','blue');
+    }
+    else{
+        i = 0;
+    }
+
+
+});
+function slideImg(){
+    setTimeout(function(){ 
+            
+        if(i < indexImg){ 
+      i++; 
+  }
+  else{ 
+      i = 0;
+  }
+
+  $img.css('display', 'none');
+  $('.cercle').css('blackground','white');
+
+  $currentImg = $img.eq(i);
+  $currentPoint = $points.eq(i);
+  $currentImg.css('display', 'block');
+  $currentPoint.css('background','blue');
+
+  slideImg(); 
+
+    }, 4000); 
+  }
+
+slideImg();
+});
+
+
+
+
+
